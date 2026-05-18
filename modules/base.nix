dccdb35b27bc8afc391b8616b7c8742ff86f2a42
Joni Hendrickson <contact@joni.site>
2026-05-17 19:12:46 -0700

    feat(base): Add states to custom fish prompt

--- modules/base.nix
+++ modules/base.nix
@@ -64,20 +64,65 @@
       fish = {
         enable = true;
         generateCompletions = true;
+        # Prefix the prompt with (ssh)(db)(nix) tags when the corresponding
+        # context applies. Order is fixed: outer to inner.
+        functions.fish_prompt = ''
+          # Capture status/pipestatus first; anything below resets $status.
+          set -l last_pipestatus $pipestatus
+          set -lx __fish_last_status $status
+
+          set -l shown 0
+          if set -q SSH_CONNECTION
+            set_color yellow
+            echo -n '(ssh)'
+            set_color normal
+            set shown 1
+          end
+          if set -q CONTAINER_ID
+            set_color red
+            echo -n '(db)'
+            set_color normal
+            set shown 1
+          end
+          if set -q IN_NIX_SHELL
+            set_color blue
+            echo -n '(nix)'
+            set_color normal
+            set shown 1
+          end
+          if test $shown -eq 1
+            echo -n ' '
+          end
+
+          # Inlined verbatim from fish's default prompt
+          # ($__fish_data_dir/tools/web_config/sample_prompts/default.fish).
+          set -l normal (set_color --reset)
+          set -l color_cwd $fish_color_cwd
+          set -l suffix '>'
+          if functions -q fish_is_root_user; and fish_is_root_user
+            if set -q fish_color_cwd_root
+              set color_cwd $fish_color_cwd_root
+            end
+            set suffix '#'
+          end
+          set -l bold_flag --bold
+          set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+          if test $__fish_prompt_status_generation = $status_generation
+            set bold_flag
+          end
+          set __fish_prompt_status_generation $status_generation
+          set -l status_color (set_color $fish_color_status)
+          set -l statusb_color (set_color $bold_flag $fish_color_status)
+          set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+          echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+        '';
         interactiveShellInit = ''
           fish_config theme choose base16-default
-          fish_config prompt choose default
-          # Make `nix develop` / `nix shell` launch fish instead of bash
-          nix-your-shell fish | source
-          # Prefix the prompt with "(nix)" when in a nix shell
-          functions -c fish_prompt _default_fish_prompt
-          function fish_prompt
-            if set -q IN_NIX_SHELL
-              set_color blue
-              echo -n "(nix) "
-              set_color normal
-            end
-            _default_fish_prompt
+          # Show "*" for unstaged and "+" for staged changes after the git branch name.
+          set -g __fish_git_prompt_showdirtystate yes
+          # Make `nix develop` / `nix shell` launch fish instead of bash.
+          if type -q nix-your-shell
+            nix-your-shell fish | source
           end
           # Force nvim as editor
           set -gx EDITOR nvim
