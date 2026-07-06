69272ffb600b3791205a2d8b94f86ec90d657ed1
Joni Hendrickson <contact@joni.site>
2026-07-06 14:54:11 -0700

    Enable ignorePerms for 0notes Syncthing folder
    Linux file ownership and permissions cannot be represented on Android's
    filesystem, causing 'access denied' out-of-sync errors. ignorePerms lets
    the destination folder's own permissions apply instead.

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -37,6 +37,7 @@ _:
       folders."~/0notes" = {
         id = "0notes";
         order = "newest";
+        ignorePerms = true;
         versioning = {
           type = "staggered";
           params.maxAge = "31536000"; # 1 year in seconds
