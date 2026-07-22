2ecd689c858da47045431afb70f05b9c2e286f44
Joni Hendrickson <contact@joni.site>
2026-07-22 16:46:01 -0700

    hm: Reorganize home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,45 +7,18 @@
   pkgs,
   ...
 }: {
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "26.05";
+  programs.home-manager.enable = true;
+  news.display = "silent";
+
+  home.username = lib.mkDefault "joni";
+  home.homeDirectory = lib.mkDefault "/home/joni";
+
+  programs.git.enable = true;
+
   # You can import other home-manager modules here
   imports = [
     ./librewolf.nix
   ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  # TODO: Set your username
-  home = {
-    username = "joni";
-    homeDirectory = "/home/joni";
-  };
-
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "26.05";
 }
