0b2cca033a35eae5694e2eb82ce18783ed745875
Joni Hendrickson <contact@joni.site>
2026-08-09 08:50:09 -0700

    scripts: Add jq dep

--- home-manager/scripts.nix
+++ home-manager/scripts.nix
@@ -1,11 +1,13 @@
 { pkgs, ... }: {
-  home.packages = builtins.attrValues (
-    builtins.mapAttrs (
-      name: _type:
-      let
-        cmd = pkgs.lib.strings.removeSuffix ".sh" name;
-      in
-      pkgs.writeShellScriptBin cmd (builtins.readFile (./scripts + "/${name}"))
-    ) (builtins.readDir ./scripts)
-  );
+  home.packages =
+    [ pkgs.jq ]
+    ++ builtins.attrValues (
+      builtins.mapAttrs (
+        name: _type:
+        let
+          cmd = pkgs.lib.strings.removeSuffix ".sh" name;
+        in
+        pkgs.writeShellScriptBin cmd (builtins.readFile (./scripts + "/${name}"))
+      ) (builtins.readDir ./scripts)
+    );
 }
