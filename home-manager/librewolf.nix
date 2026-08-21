2f4d28ebb73c7ed9aa664587b3d4aaf5e91ba73e
Joni Hendrickson <contact@joni.site>
2026-08-20 19:03:43 -0700

    librewolf: Enable firefox sync out of the box

--- home-manager/librewolf.nix
+++ home-manager/librewolf.nix
@@ -6,9 +6,13 @@
 {
   programs.librewolf = {
     enable = true;
+    policies = {
+      DisableFirefoxAccounts = false;
+    };
     settings = {
       "webgl.disabled" = false;
       "privacy.resistFingerprinting" = false;
+      "identity.fxaccounts.enabled" = true;
     };
   };
 }
