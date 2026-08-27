facfc70cee2c75f8e428236c674cf57d065630b3
Joni Hendrickson <contact@joni.site>
2026-08-27 16:47:33 -0700

    opentofu: Manage tailscale dns and tailnet settings

--- opentofu/infra/tailscale.tf
+++ opentofu/infra/tailscale.tf
@@ -1,8 +1,46 @@
 # Tailscale tailnet resources.
 
-# The tailnet policy file. Singleton resource; the import ID is arbitrary.
+# ---------------------------------------------------------
+# ACL
+#
+# Manages the tailnet policy file, stored in files/tailnet-policy.hujson.
+
 resource "tailscale_acl" "this" {
   acl = file("${path.module}/files/tailnet-policy.hujson")
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# ---------------------------------------------------------
+# DNS
+
+resource "tailscale_dns_preferences" "this" {
+  # MagicDNS is on (which matches defaults)
+  magic_dns = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# ---------------------------------------------------------
+# Tailnet settings
+
+resource "tailscale_tailnet_settings" "this" {
+  devices_approval_on                             = false
+  devices_auto_updates_on                         = true
+  # TODO: Tag servers to prevent them from expiring
+  # https://tailscale.com/docs/features/session-expiry
+  # https://tailscale.com/blog/tagged-key-expiry
+  devices_key_duration_days                       = 180
+  users_approval_on                               = false
+  users_role_allowed_to_join_external_tailnet     = "admin"
+  network_flow_logging_on                         = false
+  regional_routing_on                             = false
+  posture_identity_collection_on                  = false
+  https_enabled                                   = true
 
   lifecycle {
     prevent_destroy = true
