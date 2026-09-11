85f9925764849f2e72e69a78c8a1d36123592fa6
Joni Hendrickson <contact@joni.site>
2026-09-11 15:11:30 -0700

    nixos: Create initial caddy module

---
+++ nix/modules/nixos/caddy.nix
@@ -0,0 +1,36 @@
+# Caddy webserver for the personal site.
+{
+  let
+    domain = "joni.site";
+  in
+  {
+    services.caddy = {
+      enable = true;
+      email = "contact@joni.site";
+      openFirewall = true;
+
+      virtualHosts.${domain}.extraConfig = ''
+        root * /var/www/${domain}
+        file_server
+        encode zstd gzip
+        header {
+          -Server
+          Strict-Transport-Security "max-age=31536000; includeSubDomains"
+          X-Content-Type-Options nosniff
+          Referrer-Policy strict-origin-when-cross-origin
+          Cache-Control "public, max-age=3600"
+        }
+      '';
+
+      # TODO: Modify the static site generator to emit br and gzip files,
+      # then restore `precompressed br gzip` on file_server.
+      virtualHosts."www.${domain}".extraConfig = ''
+        redir https://${domain}{uri} permanent
+      '';
+    };
+
+    systemd.tmpfiles.rules = [
+      "d /var/www/${domain} 0755 joni root"
+    ];
+  }
+}
